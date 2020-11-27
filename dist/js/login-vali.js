e414d9032566585a5ed2e71f26d9a1c42cc3a8f4
Samuel Bitancor Jr <BITANCOR.SAMUELJR@UE.EDU.PH>
2020-11-27 23:18:12 +0800

    Login validation

---
+++ dist/js/login-vali.js
@@ -0,0 +1,10 @@
+function loginVali(){
+    alert('line2');
+	
+	
+	var formControl = input.parentElement;
+    var small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	return false;
+	
+}
