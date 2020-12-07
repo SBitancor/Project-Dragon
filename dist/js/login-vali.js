bc07a41e56b201ea263f62cf88a9c73aa17e47a1
Samuel Bitancor Jr <BITANCOR.SAMUELJR@UE.EDU.PH>
2020-12-07 16:02:37 +0800

    Certificate issuance has been removed from navbar

--- dist/js/login-vali.js
+++ dist/js/login-vali.js
@@ -1,17 +1,14 @@
 function loginVali(){
-    
-	
-	
-	/*var formControl = input.parentElement;
+	/*
+	var formControl = input.parentElement;
     var small = formControl.querySelector('small');
 	formControl.className = 'form-control error';
 	
-	
 	$('[data-toggle="tooltip"]').tooltip(); 
+	*/
+	
+	
 	
 	alert('line2');
-	*/
 	return true;
-	
-	
 }
