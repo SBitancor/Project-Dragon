517cff561a8d416937da3dcc3a5479f799487ef1
Samuel Bitancor Jr <BITANCOR.SAMUELJR@UE.EDU.PH>
2020-12-15 01:51:11 +0800

    complete javascript validation

--- dist/js/admin-vali.js
+++ dist/js/admin-vali.js
@@ -27,10 +27,15 @@ function main(){
     var alphaNum = /^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$/i;
 	
 	var residentIDValue = document.getElementById("residentID").value;
+	var positionValue = document.getElementById("position").value;
+	var usernameValue = document.getElementById("username").value;
+	var passwordValue = document.getElementById("password").value;
+	var repeatPasswordValue = document.getElementById("repeatPassword").value;
 	
+	//Resident ID
 	if (residentIDValue == ""){
-		setErrorFor("residentID", 'residentID-tooltip', 'ResidentID cannot be blank');
-        return false;
+		setErrorFor("residentID", "residentID-tooltip", "Resident ID cannot be blank");
+		return false;
 	} else {
 		setSuccessFor("residentID", 'residentID-tooltip');
 		if (numbers.test(residentIDValue)){
@@ -41,8 +46,60 @@ function main(){
 			return false;
 		}
 	}
-	return false;
 	
+	//Position
+	if (positionValue == "0"){
+		document.getElementById('position').className = "custom-select is-invalid";
+		document.getElementById("position-tooltip").style.display = "block";
+		return false;
+	} else {
+		document.getElementById("position-tooltip").style.display = "none";
+		document.getElementById('position').className = "custom-select is-valid";
+	}
+	
+	//Username
+	if (usernameValue == ""){
+		setErrorFor("username", 'username-tooltip', 'Username cannot be blank');
+		return false;
+	} else {
+		if (alphaNum.test(usernameValue)){
+			if(usernameValue.length > 7 && usernameValue.length < 19){
+				setSuccessFor("username", "username-tooltip");
+			} else {
+				setErrorFor("username", 'username-tooltip', 'Username must be 8-18 characters');
+				return false;
+			}
+		} else {
+			setErrorFor("username", 'username-tooltip', 'Username must be alphanumeric');
+			return false;
+		}
+	}
+	
+	//Password
+	if (passwordValue == ""){
+		setErrorFor("password", 'password-tooltip', 'Password cannot be blank');
+		return false;
+	} else {
+		if (alphaNum.test(passwordValue)){
+			if(passwordValue.length > 7 && passwordValue.length < 21){
+				setSuccessFor("password", "password-tooltip");
+			} else {
+				setErrorFor("password", 'password-tooltip', 'Password must be 8-20 characters');
+				return false;
+			}
+		} else {
+			setErrorFor("password", 'password-tooltip', 'Password must be alphanumeric');
+			return false;
+		}
+	}
+	
+	if (passwordValue == repeatPasswordValue){
+		setSuccessFor("repeatPassword", "repeatPassword-tooltip");
+	} else {
+		setErrorFor("repeatPassword", 'repeatPassword-tooltip', 'Password mismatch');
+		return false;
+	}
+}
 	
 	/*
     var birthDateValue = document.getElementById("birthDate").value;
@@ -204,7 +261,5 @@ function main(){
 		}
 	}
 	*/
-	
-}
 
 //MODAL: data-dismiss="modal" data-toggle="modal"
